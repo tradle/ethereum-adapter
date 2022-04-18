fdb2f1ef71407b55be4c581c219baf3a26d8d908
Martin Heidegger <martin.heidegger@gmail.com>
2022-04-19 03:32:47 +0900

    fix: better error messages
    Making sure that the actual error & stack is passed through in order to figure out where an error in the process occurred.

--- utils.js
+++ utils.js
@@ -1,14 +1,51 @@
+const errorProps = ['message', 'code', 'stack']
+const normalizeMessage = err => {
+  if (typeof err.message === 'string') {
+    return err.message
+  }
+  if (typeof err.stack === 'string' && err.stack.length > 0) {
+    return err.stack.split('\n')[0]
+  }
+  if (err.code) {
+    return `Error Code: ${err.code}`
+  }
+  return 'Unidentified Error'
+}
+
+const normalizeError = err => {
+  if (err === undefined || err === null) {
+    return new Error('Unidentified error response.')
+  }
+  if (err instanceof Error) {
+    return err
+  }
+  if (typeof err === 'object') {
+    const nonErrorProp = Object.keys(err).find(key => !errorProps.includes(key))
+    if (nonErrorProp) {
+      return new Error(JSON.stringify(err))
+    }
+    const result = new Error(normalizeMessage(err))
+    if (err.code) {
+      result.code = err.code
+    }
+    if (err.stack) {
+      Object.defineProperty(result, 'stack', {
+        value: err.stack
+      })
+    }
+    return result
+  }
+  return new Error(String(err))
+}
+
 const normalizeEngineError = (err, response) => {
-  if (!err && response && response.error) {
-    err = response.error
+  if (err) {
+    return normalizeError(err)
   }
-
-  if (!(err instanceof Error)) {
-    if (typeof err === 'object') err = JSON.stringify(err)
-    if (typeof err === 'string') err = new Error(err)
+  if (response && response.error) {
+    return normalizeError(response.error)
   }
-
-  return err
+  return normalizeError(response)
 }
 
 const getSend = engine => payload => new Promise((resolve, reject) => {
